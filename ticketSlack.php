a18e1605b4775879493935f126b68f58d40fe4e8
domkirby <kirbydom1994@gmail.com>
2017-01-15 11:40:27 -0700

    Added comments to explain purpose

--- ticketSlack.php
+++ ticketSlack.php
@@ -1,3 +1,6 @@
 <?php
+/*
+ticketSlack.php existed in my very first iteration of this tool. I switched to ticketSlack2.php when I did a clean slate. This maps adds ticketSlack2.php to avoid confusion.
+*/
 require_once("ticketSlack2.php");
 ?>
