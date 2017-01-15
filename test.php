895dfce978c35141fed3c4553750902350d527a8
domkirby <kirbydom1994@gmail.com>
2017-01-15 14:32:08 -0700

    Fixed goofy typo

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ define('TOKEN',$_GET['s']);
 <body>
 <form action="ticketSlack2.php?s=<?php echo TOKEN; ?>" method="POST">
 <p>Ticket ID (from ticket url): <input type="text" name="id"></p>
-<p>Ticket Number (T20170101.0001): <input type="text" name="id"></p>
+<p>Ticket Number (T20170101.0001): <input type="text" name="number"></p>
 <p><input type="submit" value="Send Test" /></p>
 <p><b>Note:</b>You need to use an actual existing ticket to do this.</p>
 <p>If you want the output to display in the browser, you need to set test mode to true in config.php</p>
